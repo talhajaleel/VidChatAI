76c4f22a8922b5a221d3e8dc1225c0b3ca7995bd
talhajaleel <talha.jaleel255@gmail.com>
2023-06-10 23:47:35 +0500

    complete VIDCHATAI

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
+import sys
 
 import streamlit as st
 from youtube_transcript_api import YouTubeTranscriptApi
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
+from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
+from streamlit_chat import message
 
 # os.environ['open_api_key'] = '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'
 
 
-def gpt_interaction(video_subs):
+def gpt_interaction(video_subs, user_query):
 
     os.environ["OPENAI_API_KEY"] = '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'
 
@@ -23,32 +25,35 @@ def gpt_interaction(video_subs):
 
     text_splitter = CharacterTextSplitter(
         separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
+        chunk_size=700,
+        chunk_overlap=150,
         length_function=len,
     )
     texts = text_splitter.split_text(raw_text)
-
+    # print(texts)
     # Download embeddings from OpenAI
     embeddings = OpenAIEmbeddings()
 
     docsearch = FAISS.from_texts(texts, embeddings)
     chain = load_qa_chain(OpenAI(), chain_type="stuff")
-    query = 'what is this youtube video about and write its summary in 100 words'
-    docs = docsearch.similarity_search(query)
-    res = chain.run(input_documents=docs, question=query)
+    # query = 'what is this youtube video about and write main points in bullets'
+    docs = docsearch.similarity_search(user_query)
+    res = chain.run(input_documents=docs, question=user_query)
     return res
 
 
-st.title('Talktube')
+st.title('VIDCHATAI')
 vid_url = st.text_input('Enter your URL here')
+query = st.text_input('Your query')
 move_on = st.button('Lets go!!! ')
 if move_on and vid_url:
     vid_id = vid_url.split('v=')[1]
     srt = YouTubeTranscriptApi.get_transcript(vid_id)
     captions = [i['text'] for i in srt]
-    captions = ' '.join(captions)
-    res = gpt_interaction(captions)
+    # print(captions)
+    captions = '\n'.join(captions)
+    # curr_query = st.text_input('Ask your question')
+    res = gpt_interaction(captions, query)
     st.write(res)
-    st.stop()
 
+sys.exit()
