374868f01dacf229effe5bf64c5e6ccc173f1b39
talhajaleel <talha.jaleel255@gmail.com>
2023-06-11 00:34:20 +0500

    updated changes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from langchain.llms import OpenAI
 
 def gpt_interaction(video_subs, user_query):
 
-    os.environ["OPENAI_API_KEY"] = 'your-open-api-key'
+    os.environ["OPENAI_API_KEY"] = 'your-openai-api-key'
 
     # read data from the file and put them into a variable called raw_text
     raw_text = video_subs
@@ -22,8 +22,8 @@ def gpt_interaction(video_subs, user_query):
 
     text_splitter = CharacterTextSplitter(
         separator="\n",
-        chunk_size=700,
-        chunk_overlap=150,
+        chunk_size=800,
+        chunk_overlap=100,
         length_function=len,
     )
     texts = text_splitter.split_text(raw_text)
